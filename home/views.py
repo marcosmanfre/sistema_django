6bd5412c439643ce89bf6f0d2b5a0413a6d56a9b
Marcos A. Manfre <marcosmanfre@hotmail.com>
2023-04-02 14:29:33 -0300

    atualizar home

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 def home(request):
